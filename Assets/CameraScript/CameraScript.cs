e8f61d1d30ee4c5d0f00d6ecb38b41ea35f3a300
レミィ・S <takumi1371@gmail.com>
2019-12-04 18:25:00 +0900

    新規

---
+++ Assets/CameraScript/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    float sin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sin = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間で移動する
+        sin = Mathf.Sin(Time.deltaTime);
+
+        transform.rotation = Quaternion.AngleAxis(sin, transform.position);
+    }
+}
