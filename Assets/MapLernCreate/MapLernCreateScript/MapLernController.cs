4a5ef7b5968a6d293aafe8497d8e770b7b1f300e
kbataNtsuki <konatsu1118@icloud.com>
2019-12-11 17:13:28 +0900

    レーン見た目、動き
    次回インスタンス、デストロイ

---
+++ Assets/MapLernCreate/MapLernCreateScript/MapLernController.cs
@@ -0,0 +1,26 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLernController : MonoBehaviour
+{
+    public GameObject MapLernMoveController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -0, 1f, 0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "MapLernExitPoint")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
