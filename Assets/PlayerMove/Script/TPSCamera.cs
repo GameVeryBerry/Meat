287cff89fd76552d3ec2985493aecf39895cc640
kyoya0427 <rcbb.kyoya27@gmail.com>
2019-12-20 09:58:59 +0900

    カメラワーク更新
    少し上から見下すカメラができないバグ修正中

--- Assets/PlayerMove/Script/TPSCamera.cs
+++ Assets/PlayerMove/Script/TPSCamera.cs
@@ -8,27 +8,45 @@ public class TPSCamera : MonoBehaviour
     Transform _player;
     [SerializeField]
     float _rotateSpeed;
-
-    float _yaw, _pitch;
+    float _rotate;
+    float _yaw;
+    float _pitch;
     Vector3 _distance;
-
+    Vector3 _targetPos;
+    Vector3 _cameraPos;
     private void Start()
     {
         _rotateSpeed = 1;
         _distance = transform.position;
+
+        _yaw = 0;
+        _pitch = 0;
+        Debug.Log(_distance);
+       
     }
 
     void Update()
     {
-
+       // Debug.Log(_targetPos);
+        Debug.Log(transform.position);
         //プライヤー位置を追従する
-        transform.position = new Vector3(_player.position.x + _distance.x, transform.position.y, _player.position.z + _distance.z);
+        _targetPos = _player.transform.position;
 
-        _yaw += Input.GetAxis("Mouse X") * _rotateSpeed; //横回転入力
+        float pre_yaw = _yaw;
+
+        _yaw   += Input.GetAxis("Mouse X") * _rotateSpeed; //横回転入力
         _pitch -= Input.GetAxis("Mouse Y") * _rotateSpeed; //縦回転入力
 
-        _pitch = Mathf.Clamp(_pitch, -80, 60); //縦回転角度制限する
+       _pitch = Mathf.Clamp(_pitch, -80, 60); //縦回転角度制限する
 
-        transform.eulerAngles = new Vector3(_pitch, _yaw, 0.0f); //回転の実行
+        _rotate = _yaw - pre_yaw;
+
+        transform.RotateAround(_targetPos, Vector3.up, _rotate);
+        transform.LookAt(_targetPos, Vector3.up);
+//        transform.position = _targetPos + (transform.position - _targetPos).normalized * _distance.magnitude;
+        transform.position = _targetPos + (transform.position - _targetPos + _distance).normalized * _distance.magnitude;
+
+       
+        
     }
 }
