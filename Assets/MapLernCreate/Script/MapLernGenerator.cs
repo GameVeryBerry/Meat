a0b6125c011c369b4f8ce45dffafae174030b78b
kbataNtsuki <konatsu1118@icloud.com>
2019-12-24 18:46:30 +0900

    バグ修正

--- Assets/MapLernCreate/Script/MapLernGenerator.cs
+++ Assets/MapLernCreate/Script/MapLernGenerator.cs
@@ -1,21 +1,30 @@
 ﻿using UnityEngine;
 
-public class MapLernGenerator: MonoBehaviour
+public class MapLernGenerator : MonoBehaviour
 {
+    public GameObject StartPoint;
     public GameObject Generator;
     public GameObject Gool;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
+
+
+
     void FixedUpdate()
     {
-        
-        GameObject obj= Instantiate(Generator, new Vector3(0.31f, 0.5f, 5.3f), Quaternion.identity);
+        float x = StartPoint.gameObject.transform.position.x;
+        float y = StartPoint.gameObject.transform.position.y;
+        float z = StartPoint.gameObject.transform.position.z;
+
+        GameObject obj = Instantiate(Generator, new Vector3(x, y, z), Quaternion.identity);
         obj.GetComponent<MapLernMoveController>().Init(Gool);
     }
-  
+
 }
