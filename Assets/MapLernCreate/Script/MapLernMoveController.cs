8a9afd1bb5591c9faaa49f3bd066925f0b84f03c
kbataNtsuki <konatsu1118@icloud.com>
2020-01-10 20:19:50 +0900

    コンクリフト解消　 プロト

--- Assets/MapLernCreate/Script/MapLernMoveController.cs
+++ Assets/MapLernCreate/Script/MapLernMoveController.cs
@@ -5,7 +5,8 @@
 public class MapLernMoveController : MonoBehaviour
 {
     // 目標地点のオブジェクト
-    public GameObject goalPoint;
+    [SerializeField]
+    private GameObject goalPoint;
 
     Vector3 move_Position;
     Vector3 toGoPoint;
@@ -15,25 +16,36 @@ public class MapLernMoveController : MonoBehaviour
     void Start()
     {
         move_Position = this.transform.position;    //移動させたいオブジェクトの座標
-        //toGoPoint = goalPoint.transform.position;   //目的地に設置したオブジェクトの座標
     }
 
     void Update()
     {
-        move_Position.z -= moveSpeed;
+        //move_Position.z -= moveSpeed;
+        float offset_x = toGoPoint.x - move_Position.x;
+        float offset_z = toGoPoint.z - move_Position.z;
+        float rad = Mathf.Atan2(offset_z, offset_x);
+
+        float vel_x = moveSpeed * Mathf.Cos(rad);
+        float vel_z = moveSpeed * Mathf.Sin(rad);
+
+        move_Position.x += vel_x;
+        move_Position.z += vel_z;
         this.transform.position = move_Position;
     }
 
-    public void Init(GameObject gool)
+    public void Init(GameObject goal)
     {
-        toGoPoint = gool.transform.position;   //目的地に設置したオブジェクトの座標
+        toGoPoint = goal.transform.position;   //目的地に設置したオブジェクトの座標
     }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "MapLernExitPoint")
         {
             Destroy(this.gameObject);
+
         }
     }
 }
